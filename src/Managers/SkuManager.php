353b136529d8e6ae8256871f426965943b800d68
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/SkuManager.php
+++ src/Managers/SkuManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Sku newEntity()
+ * @method \Amethyst\Schemas\SkuSchema getSchema()
+ * @method \Amethyst\Repositories\SkuRepository getRepository()
+ * @method \Amethyst\Serializers\SkuSerializer getSerializer()
+ * @method \Amethyst\Validators\SkuValidator getValidator()
+ * @method \Amethyst\Authorizers\SkuAuthorizer getAuthorizer()
+ */
 class SkuManager extends Manager
 {
     use ConfigurableManager;
